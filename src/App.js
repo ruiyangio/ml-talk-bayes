f65e2993638c57a4c6260ba91cef19bf6e0e9b8b
ruiyangio <ruiyangwind@gmail.com>
2018-06-20 16:06:26 -0700

    add math equation support

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import Reveal from 'reveal.js';
+import MathJax from 'react-mathjax2';
 import '../node_modules/reveal.js/css/reveal.css';
 import '../node_modules/reveal.js/css/theme/moon.css';
 import './App.css';
 
+const bayesTherom = 'P(A|B) = (P(B|A) * P(A))/(P(B))';
+const featureVector = 'F = (f_1, f_2, ..., f_n)';
+const featureBayes =
+  'obrace(P(C_k|F))^("posterior") = (obrace(P(F|C_k))^("likelihood") * obrace(P(C_k))^("prior"))/(ubrace(P(F))_("marginal likelihood"))';
+
 class App extends Component {
   componentDidMount() {
-    Reveal.initialize();
+    Reveal.initialize({
+      width: '100%',
+      height: '100%'
+    });
   }
 
   render() {
@@ -14,11 +23,70 @@ class App extends Component {
       <div className="demo reveal">
         <div className="slides">
           <section>
-            <header className="main-header">S1 Title</header>
-            <div className="slide-body">S1</div>
+            <header>
+              <h2>Learning session: Naive Bayes</h2>
+            </header>
+            <div>
+              <MathJax.Context input="ascii">
+                <div>
+                  <MathJax.Node>{bayesTherom}</MathJax.Node>
+                </div>
+              </MathJax.Context>
+            </div>
           </section>
-          <section>S2</section>
-          <section>S3</section>
+          <section>
+            <header>
+              <h3>Baidu Search Query Classification</h3>
+            </header>
+            <section>
+              <div>尼桑leaf的最新价格 -> Car -> Electric car</div>
+              <div>新版本炉石 红龙还好用不 -> Game -> Mobile game</div>
+            </section>
+          </section>
+          <section>
+            <header>
+              <h3>Bayes Therom and Probabilistic model</h3>
+            </header>
+            <section>
+              <div>
+                <MathJax.Context input="ascii">
+                  <div>
+                    <MathJax.Node>{featureVector}</MathJax.Node>
+                  </div>
+                </MathJax.Context>
+              </div>
+              <br />
+              <div>
+                <MathJax.Context input="ascii">
+                  <div>
+                    <MathJax.Node>{featureBayes}</MathJax.Node>
+                  </div>
+                </MathJax.Context>
+              </div>
+            </section>
+          </section>
+          <section>
+            <header>
+              <h3>Three mostly used models</h3>
+            </header>
+            <section>Gaussian bayes</section>
+            <section>Multinomial bayes</section>
+            <section>Binomial bayes</section>
+          </section>
+          <section>
+            <header>
+              <h3>Sentiment Analysis</h3>
+            </header>
+          </section>
+          <section
+            data-background-iframe="http://elincia.westus2.cloudapp.azure.com/graphql"
+            data-background-interactive
+          />
+          <section>
+            <header>
+              <h3>Evaluation</h3>
+            </header>
+          </section>
         </div>
       </div>
     );
